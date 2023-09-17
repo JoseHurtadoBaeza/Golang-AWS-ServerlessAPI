455c3b24570babfec750462cd41d2ba3de65b7f9
JoseHurtadoBaeza <josehurtadobaeza@gmail.com>
2023-09-17 05:48:45 +0200

    Rest of the Code: Developing

---
+++ pkg/user/user.go
@@ -0,0 +1,64 @@
+package user
+
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	ErrorFailedToFetchRecord     = "failed to fetch record"
+	ErrorFailedToUnmarshalRecord = "failed to unmarshal record"
+)
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*User, error) {
+
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+
+	result, err := dynaClient.GetItem(input)
+
+	if err != nil {
+		return nil, errors.New(ErrorFailedToFetchRecord)
+	}
+
+	item := new(User)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item) // Unmarshal to convert the JSON to a struct in this case
+	if err != nil {
+		return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+
+	return item, nil
+
+}
+
+func FetchUsers() {
+
+}
+
+func CreateUser() {
+
+}
+
+func UpdateUser() {
+
+}
+
+func DeleteUser() error {
+
+}
